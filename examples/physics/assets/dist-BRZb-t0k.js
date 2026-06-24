55a58d7febdd8884a194cbf9eb91196b1b9c1fbe
forgeax <forgeax@users.noreply.github.com>
2026-06-24 17:59:58 +0000

    Rebuild live demos from engine

--- examples/physics/assets/dist-BRZb-t0k.js
+++
@@ -1,1 +0,0 @@
-import{p as e,v as t}from"./dist-DU0ejTh0.js";import{t as n}from"./dist-4DaoRat1.js";import{a as r,c as i,n as a,o,r as s,s as c,t as l}from"./dist-Dwxc7JsD.js";export{n as RhiErrorClass,l as _internal_getRawDevice,a as acquireCanvasContext,s as createShaderModule,e as err,t as ok,r as requestAdapter,o as requestDevice,c as rhi,i as translateErrorEventToRhiError};
